a44cdae510c2e88297228cb99983a892a028f448
zhujianhao <yunshentech@126.com>
2021-02-03 10:53:36 +0800

    'firstcommit'

---
+++ src/main/java/com/zjh/stock/StockPredictApplication.java
@@ -0,0 +1,13 @@
+package com.zjh.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPredictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockPredictApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zjh/stock/StockPredictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjh.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPredictApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
